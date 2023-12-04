4ead6dad61ea686469b0dbdca9b7e34e1a0399ac
Manivannan <114678714+Manivannan0734@users.noreply.github.com>
2023-12-04 13:06:12 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+
 import Header from "@/comp/Header"
 import Welcome from "@/comp/Welcome"
 import Avatar from "@/comp/Avatar"
@@ -5,17 +6,31 @@ import Intro from "@/comp/Intro"
 import RecentWork from "@/comp/RecentWork"
 import Footer from "@/comp/Footer"
 import Testimonials from "@/comp/Testimonials"
+import Head from "next/head"
+import AnalyticsProvider from "@/comp/AnalyticsProvider"
 export default function Home() {
   return (
-    <div>
-      <Header/>
     
+    <div>
+      <Head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-MW9LN2RB39`}
+        />
+        <script>{`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-MW9LN2RB39');`}</script>
+      </Head>
+      <AnalyticsProvider>
+      <Header/>
       <Intro/>
       <Avatar/>
       <Welcome/>
       <RecentWork/>
       <Testimonials/>
       <Footer/>
+      </AnalyticsProvider>
     </div>
   )
 }
