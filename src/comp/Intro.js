20519bc4f2bfbbbffc7be41a0c167302f2b16875
Manivannan <114678714+Manivannan0734@users.noreply.github.com>
2024-02-03 16:04:47 +0530

    Update Intro.js

--- src/comp/Intro.js
+++ src/comp/Intro.js
@@ -6,7 +6,7 @@ const Intro = () => {
   return (
     <div className={styles.container} >
             <div>
-                <h1 className={styles.nfont} > <span style={{color:"teal"}} > Web Developer</span>, Designer & Team Player  </h1>
+                <h1 className={styles.nfont} > <span style={{color:"teal"}} > Mern Stack Developer</span>, Designer & Team Player  </h1>
             </div>
             <div>
                 <h2>I design and code beautifully simple things, and <span style={{color:"teal",marginBottom:"10rem"}}  > I love what I do.</span></h2>
